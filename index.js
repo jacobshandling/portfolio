7ed02dc73d4d6188cfd08b427245276e9b23fc80
Jacob Shandling <jacobshandling@pm.me>
2022-04-05 15:31:05 -0700

    Animate contact link highlighting on contact nav click

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const footer = document.querySelector('footer');
-const date = new Date();
-footer.innerHTML = `<p>© Jacob Shandling ${date.getFullYear()}</p>`;
+function setCopyright() {
+    const footer = document.querySelector('footer');
+    const date = new Date();
+    footer.innerHTML = `<p>© Jacob Shandling ${date.getFullYear()}</p>`;
+}
+
+function highlightElement(element) {
+    const origClass = element.className;
+    setTimeout(() => {element.className = "highlight-animate";}, 100)
+    setTimeout(() => {element.className = origClass;}, 1700);
+}
+
+function addContactAnimation() {
+    const cLinks = document.querySelector("#contact-links");
+    const navLink = document.querySelector("#contact-nav-link");
+    navLink.onclick = () => {
+        highlightElement(cLinks);
+    };
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    var testvar = 'hello';
+    addContactAnimation()
+    setCopyright();
+});
